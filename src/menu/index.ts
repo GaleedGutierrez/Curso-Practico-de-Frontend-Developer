9c4e2df33b958a7dbfe692106fb1e51417922876
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-10-15 03:00:20 -0300

    :white_check_mark: NCPDJ Clase N°20: Carrito de compras

--- src/menu/index.ts
+++ src/menu/index.ts
@@ -1,26 +1,49 @@
-const toggleDesktopMenu = () => {
-    desktopMenu.classList.toggle('inactive');
+const toggleMobileDesktopMenu = (event: Event) => {
+    const ELEMENT = event.srcElement as HTMLElement;
+    const CLASS_ELEMENT = ELEMENT.getAttribute('class');
+    const DESKTOP_CLASS = 'header__content header__item navbar-right__email';
+    const MENU_CLASS = null;
+
+    if (CLASS_ELEMENT === DESKTOP_CLASS)
+        DESKTOP_MENU.classList.toggle('inactive');
+    if (!MENU_CLASS)
+        MOBILE_MENU.classList.toggle('inactive');
+    removeCartAside();
 };
 
-const toggleMobileMenu = () => {
-    mobileMenu.classList.toggle('inactive');
+const toggleCartAside = () => {
+    ASIDE.classList.toggle('inactive');
+    removeMenuOption();
 };
 
 const removeMenuOption = () => {
-    if (!desktopMenu.classList.contains('inactive')) {
-        desktopMenu.classList.add('inactive');
+    const IS_MOBILE_MENU_CLOSE = DESKTOP_MENU.classList.contains('inactive');
+    const IS_DESKTOP_MENU_CLOSE = MOBILE_MENU.classList.contains('inactive');
+    if (!IS_MOBILE_MENU_CLOSE) {
+        DESKTOP_MENU.classList.add('inactive');
     }
-    if (!mobileMenu.classList.contains('inactive')) {
-        mobileMenu.classList.add('inactive');
+    if (!IS_DESKTOP_MENU_CLOSE) {
+        MOBILE_MENU.classList.add('inactive');
     }
 };
 
-const menuEmail = document.querySelector('.navbar-right__email') as HTMLDListElement;
-const desktopMenu = document.querySelector('.desktop-menu') as HTMLDivElement;
-const burgerIcon = document.querySelector('.header__menu') as HTMLElement;
-const mobileMenu = document.querySelector('.mobile-menu') as HTMLDivElement;
-const mobileScreen = window.matchMedia('(min-width: 800px)');
+const removeCartAside = () => {
+    const IS_CART_ASIDE_CLOSE = ASIDE.classList.contains('inactive');
+    if (!IS_CART_ASIDE_CLOSE) {
+        ASIDE.classList.add('inactive');
+    }
+};
 
-menuEmail.addEventListener('click', toggleDesktopMenu);
-burgerIcon.addEventListener('click', toggleMobileMenu);
-mobileScreen.addEventListener('change', removeMenuOption);
+const MENU_EMAIL = document.querySelector('.navbar-right__email') as HTMLDListElement;
+const DESKTOP_MENU = document.querySelector('.desktop-menu') as HTMLDivElement;
+const BURGER_ICON = document.querySelector('.header__menu') as HTMLElement;
+const MOBILE_MENU = document.querySelector('.mobile-menu') as HTMLDivElement;
+const MOBILE_SCREEN = window.matchMedia('(min-width: 800px)');
+const MENU_CART_ICON = document.querySelector('.navbar-right__shopping-cart') as HTMLDListElement;
+const ASIDE = document.querySelector('.product-detail') as HTMLElement;
+
+MENU_EMAIL.addEventListener('click', toggleMobileDesktopMenu);
+BURGER_ICON.addEventListener('click', toggleMobileDesktopMenu);
+MENU_CART_ICON.addEventListener('click', toggleCartAside);
+MOBILE_SCREEN.addEventListener('change', removeMenuOption);
+MOBILE_SCREEN.addEventListener('change', removeCartAside);
