d5f37ec046a2754efa55f51a9e8cda8151e7edc1
Galeed Gutierrez <galeedeveloper@gmail.com>
2022-10-17 00:47:53 -0300

    :white_check_mark: NCPDJ Clase N° 23: Interacción entre todos los componentes.

--- src/menu/index.ts
+++ src/menu/index.ts
@@ -1,3 +1,5 @@
+import { PRODUCT_DETAIL_CONTAINER } from '../product-card/index.js';
+
 const toggleMobileDesktopMenu = (event: Event) => {
     const ELEMENT = event.srcElement as HTMLElement;
     const CLASS_ELEMENT = ELEMENT.getAttribute('class');
@@ -12,35 +14,43 @@ const toggleMobileDesktopMenu = (event: Event) => {
 };
 
 const toggleCartAside = () => {
-    ASIDE.classList.toggle('inactive');
+    SHOPPING_CART_CONTAINER.classList.toggle('inactive');
     removeMenuOption();
 };
 
 const removeMenuOption = () => {
     const IS_MOBILE_MENU_CLOSE = DESKTOP_MENU.classList.contains('inactive');
     const IS_DESKTOP_MENU_CLOSE = MOBILE_MENU.classList.contains('inactive');
+    const IS_PRODUCT_DETAILS_CLOSE = PRODUCT_DETAIL_CONTAINER.classList.contains('inactive');
     if (!IS_MOBILE_MENU_CLOSE) {
         DESKTOP_MENU.classList.add('inactive');
     }
     if (!IS_DESKTOP_MENU_CLOSE) {
         MOBILE_MENU.classList.add('inactive');
     }
+    if (!IS_PRODUCT_DETAILS_CLOSE) {
+        PRODUCT_DETAIL_CONTAINER.classList.add('inactive');
+    }
 };
 
 const removeCartAside = () => {
-    const IS_CART_ASIDE_CLOSE = ASIDE.classList.contains('inactive');
+    const IS_CART_ASIDE_CLOSE = SHOPPING_CART_CONTAINER.classList.contains('inactive');
+    const IS_PRODUCT_DETAILS_CLOSE = PRODUCT_DETAIL_CONTAINER.classList.contains('inactive');
     if (!IS_CART_ASIDE_CLOSE) {
-        ASIDE.classList.add('inactive');
+        SHOPPING_CART_CONTAINER.classList.add('inactive');
+    }
+    if (!IS_PRODUCT_DETAILS_CLOSE) {
+        PRODUCT_DETAIL_CONTAINER.classList.add('inactive');
     }
 };
 
 const MENU_EMAIL = document.querySelector('.navbar-right__email') as HTMLDListElement;
-const DESKTOP_MENU = document.querySelector('.desktop-menu') as HTMLDivElement;
+export const DESKTOP_MENU = document.querySelector('.desktop-menu') as HTMLDivElement;
 const BURGER_ICON = document.querySelector('.header__menu') as HTMLElement;
-const MOBILE_MENU = document.querySelector('.mobile-menu') as HTMLDivElement;
+export const MOBILE_MENU = document.querySelector('.mobile-menu') as HTMLDivElement;
 const MOBILE_SCREEN = window.matchMedia('(min-width: 800px)');
 const MENU_CART_ICON = document.querySelector('.navbar-right__shopping-cart') as HTMLDListElement;
-const ASIDE = document.querySelector('.product-detail') as HTMLElement;
+export const SHOPPING_CART_CONTAINER = document.querySelector('.product-detail') as HTMLElement;
 
 MENU_EMAIL.addEventListener('click', toggleMobileDesktopMenu);
 BURGER_ICON.addEventListener('click', toggleMobileDesktopMenu);
